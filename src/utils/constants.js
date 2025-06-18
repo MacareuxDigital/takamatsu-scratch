084ffdda0b869b23ab40f7c737ff6283ba1fe10a
mimi <mimitips@gmail.com>
2025-06-18 17:28:29 +0900

    env: change the default baseApiUrl value to production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,16 @@
 /* this baseApiUrl is temporary, it should be replaced by a method using the .env file. */
 let baseApiUrl;
 
-if (process.env.NODE_ENV === 'production') {
-    // Production specific settings
-    baseApiUrl = 'https://chizubouken-lab.jp';
+if (process.env.NODE_ENV === 'development') {
+    // Local development settings
+    baseApiUrl = process.env.BASE_URL || 'http://localhost:8310';
 /* Use the following lines if we can set a different env for development and local */
 // } else if (process.env.NODE_ENV === 'development') {
 //     // Development specific settings
 //     baseApiUrl = process.env.BASE_URL || 'https://nginx.dev.adventure-lab-cms.jp1.amazee.io/';
 } else {
-    // Local development settings
-    baseApiUrl = process.env.BASE_URL || 'http://localhost:8310';
+    // Production specific settings
+    baseApiUrl = 'https://chizubouken-lab.jp';
 }
 
 export const BASE_API_URL = baseApiUrl;
