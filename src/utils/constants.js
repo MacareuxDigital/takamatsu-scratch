f63490be1da09f0e502e28972ad47d85308dcc7f
mimi <mimitips@gmail.com>
2025-06-18 16:34:10 +0900

    env, fix/#145

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,17 @@
-export const BASE_API_URL = "https://chizubouken-lab.jp";
-export const TOKEN_KEY = "access_token";
-export const REFRESH_TOKEN_KEY = "refresh_token";
+/* this baseApiUrl is temporary, it should be replaced by a method using the .env file. */
+let baseApiUrl;
+
+if (process.env.NODE_ENV === 'production') {
+    // Production specific settings
+    baseApiUrl = 'https://chizubouken-lab.jp';
+} else if (process.env.NODE_ENV === 'development') {
+    // Development specific settings
+    baseApiUrl = 'https://nginx.dev.adventure-lab-cms.jp1.amazee.io/';
+} else {
+    // Local development settings
+    baseApiUrl = process.env.BASE_URL || 'http://localhost:8310';
+}
+
+export const BASE_API_URL = baseApiUrl;
+export const TOKEN_KEY = 'access_token';
+export const REFRESH_TOKEN_KEY = 'refresh_token';
