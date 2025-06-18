091bc7398c3bcd4815c60972457c2eaa2208be3a
mimi <mimitips@gmail.com>
2025-06-18 17:22:48 +0900

    env: comment out the codes for dev server for a while

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,9 +4,10 @@ let baseApiUrl;
 if (process.env.NODE_ENV === 'production') {
     // Production specific settings
     baseApiUrl = 'https://chizubouken-lab.jp';
-} else if (process.env.NODE_ENV === 'development') {
-    // Development specific settings
-    baseApiUrl = 'https://nginx.dev.adventure-lab-cms.jp1.amazee.io/';
+/* Use the following lines if we can set a different env for development and local */
+// } else if (process.env.NODE_ENV === 'development') {
+//     // Development specific settings
+//     baseApiUrl = process.env.BASE_URL || 'https://nginx.dev.adventure-lab-cms.jp1.amazee.io/';
 } else {
     // Local development settings
     baseApiUrl = process.env.BASE_URL || 'http://localhost:8310';
